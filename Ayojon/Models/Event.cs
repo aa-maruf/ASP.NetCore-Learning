3e8e580394f6cb7fcf3540119f6cd1eb385d9b0c
Abdullah Al Maruf <aamaruf2000@gmail.com>
2025-09-10 13:51:01 +0600

    Assignemnt 2

---
+++ Ayojon/Models/Event.cs
@@ -0,0 +1,15 @@
+﻿namespace Ayojon.Models
+{
+    public class Event
+    {
+       // name, description, date, address, assigned user is neededd
+
+        public int id { get; set; }
+        public string name { get; set; }
+        public string description { get; set; } 
+        public DateTime date { get; set; }
+        public string address { get; set; }
+        public int assignedUser { get; set; }
+
+    }
+}
